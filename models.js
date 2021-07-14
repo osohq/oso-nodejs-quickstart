3086dc900b6b733eabbf5fb9cd3b3f602c5858b6
David Hatch <david@osohq.com>
2021-07-13 22:06:12 -0400

    update guest.

--- models.js
+++ models.js
@@ -1,47 +1,44 @@
 class Page {
-    pages = []
+  pages = [];
 
-    constructor(pagenum) {
-        this.pagenum = pagenum;
-    }
+  constructor(pagenum) {
+    this.pagenum = pagenum;
+  }
 
-    static getPage(pagenum) {
-        const num = parseInt(pagenum)
-        if (num < Page.pages.length) {
-            return Page.pages[num]
-        } else {
-            return undefined
-        }
+  static getPage(pagenum) {
+    const num = parseInt(pagenum);
+    if (num < Page.pages.length) {
+      return Page.pages[num];
+    } else {
+      return undefined;
     }
+  }
 }
 
-Page.pages = [
-    new Page(0),
-    new Page(1),
-    new Page(2),
-]
+Page.pages = [new Page(0), new Page(1), new Page(2)];
 
 ROLES = {
-    "alice": [
-        { "name": "user", "resource": Page.pages[0] },
-        { "name": "admin", "resource": Page.pages[1] },
-    ],
-    "bob": [{ "name": "admin", "resource": Page.pages[2] }],
-}
+  alice: [
+    { name: 'guest', resource: Page.pages[0] },
+    { name: 'admin', resource: Page.pages[1] }
+  ],
+  bob: [{ name: 'admin', resource: Page.pages[2] }]
+};
 
 class User {
-    constructor(name) {
-        this.name = name;
-    }
+  constructor(name) {
+    this.name = name;
+  }
 
-    static getCurrentUser() {
-        return new User("bob")
-    }
+  static getCurrentUser() {
+    return new User('bob');
+  }
 
-    getRoles() {
-        return ROLES[this.name]
-    }
+  getRoles() {
+    return ROLES[this.name];
+  }
 }
 module.exports = {
-    Page, User
+  Page,
+  User
 };
